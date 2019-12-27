21a8ceb897c30c64826daae162a368f8da1e4f44
mark-hartmann <contact@mark-hartmann.com>
2019-12-27 18:29:49 +0100

    Created FileSystemEnvironment.cs as a later replacement for LocalEnvironment.cs

---
+++ LuaPackageOrganizer/Environments/FileSystemEnvironment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LuaPackageOrganizer.Packages;
+
+namespace LuaPackageOrganizer.Environments
+{
+    public class FileSystemEnvironment
+    {
+        public LupoJsonFile LupoJson { get; }
+        private string LupoJsonFile { get; }
+        private string VendorDirectory { get; }
+
+        protected FileSystemEnvironment(string root)
+        {
+            LupoJsonFile = Path.Join(root, "lupo.json");
+            VendorDirectory = Path.Join(root, "vendor");
+
+            if (File.Exists(LupoJsonFile) != true)
+                throw new Exception("lupo.json does not exist");
+
+            if (Directory.Exists(VendorDirectory) != true)
+                throw new Exception("vendor directory does not exist");
+
+            LupoJson = LuaPackageOrganizer.LupoJsonFile.ParseFile(LupoJsonFile);
+        }
+
+        public static FileSystemEnvironment Local()
+        {
+            // todo: Change to Directory.GetCurrentDirectory()
+            return new FileSystemEnvironment(@"C:\Users\markh\IdeaProjects\LupoTestPackage");
+        }
+
+        public bool PackageAlreadyInstalled(Package package)
+        {
+            var foundPackages = LupoJson.Packages.Where(p => p.FullName == package.FullName).ToList();
+
+            if (foundPackages.Count == 0)
+                return false;
+
+            if (foundPackages.Any(p => p.Release.Equals(package.Release) == false))
+                throw new Exception($"{package.FullName} installed with a different version ({package.Release.Name})");
+
+            return true;
+        }
+
+        private string GetInstallationDirectoryFor(Package package)
+        {
+            return Path.Join(VendorDirectory, package.Vendor, package.PackageName);
+        }
+    }
+}
