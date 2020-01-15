001a35641e1f311aa0fdc2da68f77d9eb195f786
mark-hartmann <contact@mark-hartmann.com>
2020-01-15 19:01:59 +0100

    Restructured the options

---
+++ LuaPackageOrganizer/Commands/Options/PackageOptions.cs
@@ -0,0 +1,40 @@
+﻿using CommandLine;
+
+namespace LuaPackageOrganizer.Commands.Options
+{
+    public abstract class PackageOptions
+    {
+        public string Vendor;
+        public string PackageName;
+
+        [Value(0, MetaName = "package", HelpText = "The package", Required = true)]
+        public string Package
+        {
+            get => Vendor + '/' + PackageName;
+            set
+            {
+                var split = value.Split('/');
+                Vendor = split[0];
+                PackageName = split[1];
+            }
+        }
+
+        [Option('p', "project", Default = null)]
+        public string ProjectDirectory { get; set; }
+    }
+
+    [Verb("install", HelpText = "Install a package to your project")]
+    public class InstallOptions : PackageOptions
+    {
+        [Value(1, MetaName = "release", HelpText = "The release you want to install", Required = false)]
+        public string Release { get; set; }
+
+        [Option("no-release", HelpText = "Uses the packages active branch (when applicable)", Default = false)]
+        public bool UseActiveBranch { get; set; }
+    }
+
+    [Verb("remove", HelpText = "removes a package from your project")]
+    public class RemoveOptions : PackageOptions
+    {
+    }
+}
--- LuaPackageOrganizer/Terminal.cs
+++ LuaPackageOrganizer/Terminal.cs
@@ -36,6 +36,5 @@ namespace LuaPackageOrganizer
 
     class Terminal
     {
-        
     }
 }
