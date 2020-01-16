c3b03b551090c03666fd9c2b4d3521596604aab1
mark-hartmann <contact@mark-hartmann.com>
2020-01-16 21:15:11 +0100

    HelpText improved

--- LuaPackageOrganizer/Commands/Options/PackageOptions.cs
+++ LuaPackageOrganizer/Commands/Options/PackageOptions.cs
@@ -19,21 +19,25 @@ namespace LuaPackageOrganizer.Commands.Options
             }
         }
 
-        [Option('p', "project", Default = null)]
+        [Option('p', "project",
+            HelpText = "The path to the project directory. If not given the current directory is used ",
+            Default = null)]
         public string ProjectDirectory { get; set; }
     }
 
     [Verb("install", HelpText = "Install a package to your project")]
     public class InstallOptions : PackageOptions
     {
-        [Value(1, MetaName = "release", HelpText = "The release you want to install", Required = false)]
+        [Value(1, MetaName = "release",
+            HelpText = "The release to install, the latest version is used if not specified otherwise",
+            Required = false)]
         public string Release { get; set; }
 
         [Option("no-release", HelpText = "Uses the packages active branch (when applicable)", Default = false)]
         public bool UseActiveBranch { get; set; }
     }
 
-    [Verb("remove", HelpText = "removes a package from your project")]
+    [Verb("remove", HelpText = "Remove package from your project")]
     public class RemoveOptions : PackageOptions
     {
     }
