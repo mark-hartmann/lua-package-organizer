72ed7454322fc26593b321f5d5c2115c424e914c
mark-hartmann <contact@mark-hartmann.com>
2020-01-18 23:18:06 +0100

    More documentation

--- LuaPackageOrganizer/Environments/IPackageManager.cs
+++ LuaPackageOrganizer/Environments/IPackageManager.cs
@@ -4,6 +4,9 @@ namespace LuaPackageOrganizer.Environments
 {
     public interface IPackageManager
     {
+        /// <summary>
+        /// If the environments state changed (eg. by installing or removing a package)
+        /// </summary>
         public bool IsModified { get; }
 
         /// <summary>
@@ -14,8 +17,19 @@ namespace LuaPackageOrganizer.Environments
         /// <returns></returns>
         public bool IsInstalled(Package package, bool explicitly = false);
 
+        /// <summary>
+        /// Returns if the given package has any dependents
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns></returns>
         public bool HasDependents(Package package);
 
+        /// <summary>
+        /// Installs a given package to the project
+        /// </summary>
+        /// <param name="package"></param>
+        /// <param name="repository">The repository to obtain the package from</param>
+        /// <param name="explicitly">If true, the package is written to the lupo.json as well</param>
         public void Install(Package package, IRepository repository, bool explicitly = false);
 
         /// <summary>
@@ -26,10 +40,23 @@ namespace LuaPackageOrganizer.Environments
         /// <param name="explicitly">If true, the package also gets removed from the lupo.json</param>
         public void Uninstall(Package package, bool explicitly = false);
 
+        /// <summary>
+        /// This finishes a change in the environment, meaning if a package was installed, this method persists the
+        /// lupo.lock and lupo.json file
+        /// </summary>
         public void ApplyChanges();
 
+        /// <summary>
+        /// If something went wrong, this cleans up the environment
+        /// </summary>
         public void RollbackChanges();
 
+        /// <summary>
+        /// Returns the installation path for the given package
+        /// </summary>
+        /// <example>yonaba/30log @ 30log-1.3.0-1 results in C:/.../project/vendor/yonaba/30log</example>
+        /// <param name="package"></param>
+        /// <returns></returns>
         public string InstallPath(Package package);
     }
 }
