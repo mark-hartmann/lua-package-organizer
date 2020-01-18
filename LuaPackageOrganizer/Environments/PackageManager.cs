2958e573e28e11d0734e73811d541d6411c5ac73
mark-hartmann <contact@mark-hartmann.com>
2020-01-18 22:35:50 +0100

    Created basic implementation of IPackageManager.cs

---
+++ LuaPackageOrganizer/Environments/PackageManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LuaPackageOrganizer.Packages;
+
+namespace LuaPackageOrganizer.Environments
+{
+    public class PackageManager : IPackageManager
+    {
+        private readonly LupoJsonFile _lupoJson;
+        private readonly LupoLockFile _lupoLock;
+        private readonly FileSystemEnvironment _env;
+
+        public PackageManager(FileSystemEnvironment environment)
+        {
+            IsModified = false;
+
+            _env = environment;
+            _lupoJson = LupoJsonFile.ParseFile(environment.LupoJsonFile);
+            _lupoLock = LupoLockFile.ParseFile(environment.LupoJsonFile);
+        }
+
+        public bool IsModified { get; private set; }
+
+        public bool IsInstalled(Package package, bool explicitly = false)
+        {
+            // Should check if already installed but using a different release
+            return explicitly
+                ? _lupoJson.Packages.Contains(package)
+                : _lupoLock.GetPackages().Any(p => p.FullName == package.FullName);
+        }
+
+        public void Install(Package package, IRepository repository, bool explicitly = false)
+        {
+            if (repository.HasPackage(package) == false)
+            {
+                throw new PackageNotFoundException(package);
+            }
+
+            if (explicitly)
+            {
+                _lupoJson.AddPackage(package);
+            }
+
+            IsModified = true;
+            _lupoLock.LockPackage(package, repository);
+
+            repository.DownloadFiles(package, InstallPath(package));
+        }
+
+        public void Uninstall(Package package, bool explicitly = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ApplyChanges()
+        {
+            if (!IsModified)
+            {
+                return;
+            }
+
+            _lupoJson.WriteChanges();
+            _lupoLock.WriteChanges();
+        }
+
+        public void RollbackChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string InstallPath(Package package) =>
+            Path.Join(_env.VendorDirectoryPath, package.Vendor, package.FullName);
+    }
+}
