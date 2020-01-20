f0fc8518af1e122cf54c49c29df0d972c03205ac
mark-hartmann <contact@mark-hartmann.com>
2020-01-20 19:30:15 +0100

    Added InitOptions.cs

---
+++ LuaPackageOrganizer/Commands/Options/InitOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace LuaPackageOrganizer.Commands.Options
+{
+    [Verb("init", HelpText = "Initialize a new project environment")]
+    public class InitOptions
+    {
+        [Option('p', "project", HelpText = "The path where the project is going to be", Default = null)]
+        public string ProjectDirectory { get; set; }
+    }
+}
