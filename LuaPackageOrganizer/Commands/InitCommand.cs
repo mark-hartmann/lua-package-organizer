a4075a735861fd9177ee1d2a6b8a27afac2e874d
mark-hartmann <contact@mark-hartmann.com>
2020-01-20 19:52:21 +0100

    Implemented basic functionality for InitCommand.cs

--- LuaPackageOrganizer/Commands/InitCommand.cs
+++ LuaPackageOrganizer/Commands/InitCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using LuaPackageOrganizer.Commands.Options;
+using LuaPackageOrganizer.Environments;
 
 namespace LuaPackageOrganizer.Commands
 {
@@ -7,7 +9,23 @@ namespace LuaPackageOrganizer.Commands
     {
         public void Execute(InitOptions options)
         {
-            Console.WriteLine("Initializing project...");
+            try
+            {
+                if (!Directory.Exists(options.ProjectDirectory))
+                {
+                    throw new DirectoryNotFoundException($"Directory \"{options.ProjectDirectory}\" does not exist.");
+                }
+
+                Console.WriteLine("Initialize project...");
+                FileSystemEnvironment.Init(options.ProjectDirectory);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                Console.WriteLine();
+                Console.WriteLine("Initialisation failed");
+            }
         }
     }
 }
