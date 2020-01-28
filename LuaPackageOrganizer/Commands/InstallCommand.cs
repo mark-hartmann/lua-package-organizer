2dd44894efdcb51bc3ce183501437c6ce0d602c3
mark-hartmann <contact@mark-hartmann.com>
2020-01-28 19:53:23 +0100

    Made _environment and _repository local variables

--- LuaPackageOrganizer/Commands/InstallCommand.cs
+++ LuaPackageOrganizer/Commands/InstallCommand.cs
@@ -12,17 +12,10 @@ namespace LuaPackageOrganizer.Commands
 {
     public class InstallCommand
     {
-        private GithubRepository _repository;
-        private FileSystemEnvironment _environment;
-
-        public InstallCommand()
-        {
-            _repository = new GithubRepository();
-        }
-
         public void Execute(InstallOptions options)
         {
-            _environment = new FileSystemEnvironment(options.ProjectDirectory);
+            var environment = new FileSystemEnvironment(options.ProjectDirectory);
+            var repository = new GithubRepository();
 
             // Create package using the given options
             var package = new Package(options.Vendor, options.PackageName, new Release {Name = options.Release});
@@ -33,7 +26,7 @@ namespace LuaPackageOrganizer.Commands
                 // This is a neat one, if the user tried to install a package without release it checks if the
                 // --no-release flag was set. If so, the package will use the projects active branch, otherwise
                 // the latest available release will be installed
-                var latestRelease = _repository.GetLatestRelease(package, options.UseActiveBranch);
+                var latestRelease = repository.GetLatestRelease(package, options.UseActiveBranch);
                 package = new Package(package.Vendor, package.PackageName, latestRelease);
 
                 var colorizedPackageName = package.FullName.Pastel(Color.CornflowerBlue);
@@ -48,22 +41,22 @@ namespace LuaPackageOrganizer.Commands
             try
             {
                 // Early exit installation if the package is already installed
-                if (_environment.PackageManager.IsInstalled(package, true))
+                if (environment.PackageManager.IsInstalled(package, true))
                 {
                     Terminal.WriteNotice($"{package.FullName.Pastel(Color.CornflowerBlue)} is already installed");
                     return;
                 }
 
-                if (!_repository.IsReleaseAvailable(package, package.Release))
+                if (!repository.IsReleaseAvailable(package, package.Release))
                 {
                     throw new ReleaseNotFoundException(package);
                 }
 
                 // todo: Check if the dependencies are broken (Same package w/ different Releases) 
-                var packages = new List<Package>(_repository.GetRequiredPackages(package)) {package};
+                var packages = new List<Package>(repository.GetRequiredPackages(package)) {package};
 
                 var installationRequired =
-                    packages.Where(p => _environment.PackageManager.IsInstalled(p) == false).ToList();
+                    packages.Where(p => environment.PackageManager.IsInstalled(p) == false).ToList();
 
                 var installationNotRequired =
                     packages.Where(p => installationRequired.Contains(p) == false).ToList();
@@ -81,12 +74,12 @@ namespace LuaPackageOrganizer.Commands
                 foreach (var pkg in installationRequired)
                 {
                     var installExplicitly = pkg.Equals(package);
-                    _environment.PackageManager.Install(pkg, _repository, installExplicitly);
+                    environment.PackageManager.Install(pkg, repository, installExplicitly);
                 }
             }
             catch (ReleaseNotFoundException e)
             {
-                var availableReleases = _repository.GetAvailableReleases(e.FailedPackage);
+                var availableReleases = repository.GetAvailableReleases(e.FailedPackage);
                 Terminal.WriteNotice(e.Message + ", " + (availableReleases.Count == 0
                                          ? "no releases available."
                                          : "the following releases are available:"));
@@ -118,11 +111,11 @@ namespace LuaPackageOrganizer.Commands
                 Terminal.WriteError(e.Message);
             }
 
-            _environment.PackageManager.ApplyChanges();
+            environment.PackageManager.ApplyChanges();
 
             // If the environment was modified (added folders) but failed during the installation process, the mess must
             // be cleaned and set back to the initial state
-            if (_environment.PackageManager.IsModified)
+            if (environment.PackageManager.IsModified)
             {
                 // todo: Cleanup all the mess that was added during the process
                 // todo: Iterate through all added packages and cleanup all created directories or so 
