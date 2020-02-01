50d6b2dd444c0909d70f4c14ede78bb71c313fb2
mark-hartmann <contact@mark-hartmann.com>
2020-02-01 17:25:39 +0100

    Created BasicOutput.cs as a base for Command-dependent views

---
+++ LuaPackageOrganizer/Commands/Output/BasicOutput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Console = Colorful.Console;
+
+namespace LuaPackageOrganizer.Commands.Output
+{
+    public class BasicOutput : IOutput
+    {
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void WriteLine(string message)
+        {
+            Console.Write(message + Environment.NewLine);
+        }
+    }
+}
