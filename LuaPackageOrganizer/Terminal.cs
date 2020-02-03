c3a4955da2df2ce2d2786c45ab318d6e85b38e91
mark-hartmann <contact@mark-hartmann.com>
2020-02-03 18:31:22 +0100

    Removed Terminal methods, no longer needed

--- LuaPackageOrganizer/Terminal.cs
+++ LuaPackageOrganizer/Terminal.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
 using Pastel;
 
 namespace LuaPackageOrganizer
@@ -53,51 +51,6 @@ namespace LuaPackageOrganizer
             Notice,
             Success,
             Warning
-        }
-
-        public static void WriteError(string message, params string[] additionalMessages) =>
-            WriteLine(MessageType.Error, message, Color.Firebrick, true, additionalMessages);
-
-        public static void WriteDebug(string message, params string[] additionalMessages) =>
-            WriteLine(MessageType.Debug, message, Color.Gray, true, additionalMessages);
-
-        public static void WriteNotice(string message, params string[] additionalMessages) =>
-            WriteLine(MessageType.Notice, message, Color.LightGray, true, additionalMessages);
-
-        public static void WriteWarning(string message, params string[] additionalMessages) =>
-            WriteLine(MessageType.Warning, message, Color.Olive, true, additionalMessages);
-
-        public static void WriteSuccess(string message, params string[] additionalMessages) =>
-            WriteLine(MessageType.Success, message, Color.LightGreen, true, additionalMessages);
-
-        public static void WriteLine(MessageType? type, string message, Color textColor, bool showTime = true,
-            IEnumerable<string> additionalMessages = null)
-        {
-            Write(type, message + Environment.NewLine, textColor, showTime);
-
-            if (additionalMessages == null)
-                return;
-
-            foreach (var additionalMessage in additionalMessages)
-            {
-                Write(null, additionalMessage + Environment.NewLine, Color.LightGray, false);
-            }
-        }
-
-        public static void Write(MessageType? type, string message, Color textColor, bool showTime = true)
-        {
-            // Resolves the longest name so it can be used to set the totalWidth
-            var totalWidth = Enum.GetNames(typeof(MessageType)).Max(s => s.Length);
-            var messageTypeName = type != null ? Enum.GetName(typeof(MessageType), type) : "";
-
-            // totalWidth + 2 because of the braces around the message type
-            var messageType = $"{messageTypeName?.ToLower()}".PadLeft(totalWidth + (showTime ? 0 : 14), ' ');
-
-            if (showTime)
-                Console.Write($"{DateTime.Now:HH:mm:ss.ffff} ");
-
-            Console.Write($"{messageType} ".Pastel(Color.Olive));
-            Console.Write($"{message}".Pastel(textColor));
         }
     }
 }
